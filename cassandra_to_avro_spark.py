6489cf34fdc0457611c25acd6b7842d1271d3672
Bakos, Gabor <bakos.gabor@mind-era.com>
2021-03-27 16:25:07 +0100

    Fix spark job (using sparksession instead of ctx)

--- cassandra_to_avro_spark.py
+++ cassandra_to_avro_spark.py
@@ -7,7 +7,7 @@ spark = pyspark.sql.SparkSession(sc)
 
 # https://stackoverflow.com/a/46675457
 hosts = {"spark.cassandra.connection.host": 'host.docker.internal'}
-df = sc.read.format("org.apache.spark.sql.cassandra").options(**hosts).load(keyspace="killrvideo", table="videos")
+df = spark.read.format("org.apache.spark.sql.cassandra").options(**hosts).load(keyspace="killrvideo", table="videos")
 # df = spark.read.csv("videos.csv", header=True)
 # sch = avro.schema.make_avsc_object({
 #     'type':'record', 
